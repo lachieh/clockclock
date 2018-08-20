6899e9b85914a09826db112900e345c07af45e05
Lachlan Heywood <lachlan@heywood.net.au>
2018-08-20 12:15:15 -0400

    Update background and add props for controlling

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -23,7 +23,7 @@ class Clock extends Component {
   }
 
   render() {
-    const { hour, minute } = this.props;
+    const { hour, minute, bgX, bgY } = this.props;
 
     return (
       <div className="Clock">
@@ -32,7 +32,23 @@ class Clock extends Component {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 200 200"
         >
-          <path d="M100 200C44.9 200 0 155.1 0 100S44.9 0 100 0s100 44.9 100 100-44.9 100-100 100zm0-195.5C47.3 4.5 4.5 47.3 4.5 100s42.9 95.5 95.5 95.5c52.7 0 95.5-42.9 95.5-95.5 0-52.7-42.8-95.5-95.5-95.5z" />
+          <path
+            d="M100 3c53.5 0 97 43.5 97 97s-43.5 97-97 97-97-43.5-97-97S46.5 3 100 3m0-3C44.9 0 0 44.9 0 100s44.9 100 100 100 100-44.9 100-100S155.1 0 100 0z"
+            fill="#aaa"
+          />
+          <radialGradient
+            id="a"
+            cx={bgX}
+            cy={bgY}
+            r="102"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset=".708" stopColor="#eee" />
+            <stop offset=".806" stopColor="#e9e9e9" />
+            <stop offset=".914" stopColor="#dcdcdc" />
+            <stop offset="1" stopColor="#ccc" />
+          </radialGradient>
+          <circle cx="100" cy="100" r="98.5" fill="url(#a)" />
           <Hand short rotation={hour} />
           <Hand long rotation={minute} />
         </svg>
@@ -44,11 +60,15 @@ class Clock extends Component {
 Clock.propTypes = {
   hour: PropTypes.number,
   minute: PropTypes.number,
+  bgX: PropTypes.number,
+  bgY: PropTypes.number,
 };
 
 Clock.defaultProps = {
   hour: 0,
   minute: 0,
+  bgX: 100,
+  bgY: 105,
 };
 
 export default Clock;
