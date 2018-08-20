95ae791a67615aa8e28b5787bfddcde35eb3ef9b
Lachlan Heywood <lachlan@heywood.net.au>
2018-08-20 12:15:39 -0400

    refactor app

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,9 @@ export default class App extends Component {
 
   timeToClockDigits() {
     const now = new Date(Date.now());
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
     let h1 = 0;
     let h2 = 0;
     let m1 = 0;
@@ -32,20 +32,17 @@ export default class App extends Component {
     if (hours < 10) {
       h2 = hours;
     } else {
-      hours = hours.toString();
-      [h1, h2] = hours;
+      [h1, h2] = hours.toString();
     }
     if (minutes < 10) {
       m2 = minutes;
     } else {
-      minutes = minutes.toString();
-      [m1, m2] = minutes;
+      [m1, m2] = minutes.toString();
     }
     if (seconds < 10) {
       s2 = seconds;
     } else {
-      seconds = seconds.toString();
-      [s1, s2] = seconds;
+      [s1, s2] = seconds.toString();
     }
     this.setState({
       h1: Number(h1),
